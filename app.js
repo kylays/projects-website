38a0012a15f7e9ddf47cdc0709dd9f5336b92b99
kylays <kyla.yu.swanson@gmail.com>
2022-06-27 12:59:13 -0700

    node.js stuff

---
+++ app.js
@@ -0,0 +1,15 @@
+/* 
+ * NAME: Kyla Yu-Swanson
+ */
+
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.use(express.static("public")); 
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
