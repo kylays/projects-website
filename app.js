d157bdcf1bbc31244ef328c3cabb86af490f4b0c
kylays <kyla.yu.swanson@gmail.com>
2023-12-10 02:26:13 -0800

    puzzle description

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public")); 
+app.use(express.static("docs")); 
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
